389a0b439f4ea954264c9ce8aab6ab23e7f8baa8
Darlan Alves <me@darlanalv.es>
2024-01-22 15:12:02 +0100

    feat: add a reload button

--- src/component/header.ts
+++ src/component/header.ts
@@ -3,11 +3,11 @@ import { html } from './component';
 
 const t = html`
   <header class="flex h-12 items-center px-4 border-b">
-    <a href="#">
+    <a href="#" @click="onReload()">
       <img class="w-6 h-6 rounded" src="https://avatars.githubusercontent.com/u/69599650?s=24&v=4" alt="" />
     </a>
     <span class="text-sm flex-grow ml-2 font-bold">jsfn.run</span>
-    <button class="w-6 h-6" @click="addFile()"><span class="material-icons">add</span></button>
+    <button class="w-6 h-6" @click="onAddFile()"><span class="material-icons">add</span></button>
   </header>
 `;
 
@@ -16,8 +16,13 @@ export class Header extends HTMLElement {
     this.append(t(this));
   }
 
-  addFile() {
+  onAddFile() {
     dispatch('addfile', prompt('File name'));
+  }
+
+  onReload() {
+    dispatch('updatefilelist');
+    dispatch('updatefunctionlist');
   }
 }
 
